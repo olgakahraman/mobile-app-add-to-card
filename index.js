a0e4c7d444c9818a02fdbc98c2130c7cd33ffb42
olgakahraman <ol.khrmn@gmail.com>
2023-07-08 23:20:22 -0400

    added Firebase to project

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+  databaseURL: "https://realtime-database-812db-default-rtdb.firebaseio.com/",
+};
+
+const app  = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
+
+
+console.log(app);
+
+
 const addButtonEl = document.querySelector("#add-button");
 const inputFieldEl = document.querySelector("#input-field");
 
 addButtonEl.addEventListener("click", function(){
     
-    let inputValue = inputFieldEl.value
-    console.log(inputValue)
+    let inputValue = inputFieldEl.value;
+    push(shoppingListInDB, inputValue)
+    console.log(`${inputValue} added to database`);
 })
