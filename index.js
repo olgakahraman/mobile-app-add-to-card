c22796a39532b639b7e0465c2043ee6285cfca69
olgakahraman <ol.khrmn@gmail.com>
2023-07-08 21:53:50 -0400

    setting up app skeleton

---
+++ index.js
@@ -0,0 +1,8 @@
+const addButtonEl = document.querySelector("#add-button");
+const inputFieldEl = document.querySelector("#input-field");
+
+addButtonEl.addEventListener("click", function(){
+    
+    let inputValue = inputFieldEl.value
+    console.log(inputValue)
+})
