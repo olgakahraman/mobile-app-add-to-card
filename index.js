ec1926e598e0444480923c3a1b096e5b7386054a
olgakahraman <ol.khrmn@gmail.com>
2023-07-12 18:40:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import {
-  getDatabase,
-  ref,
-  push,
-  onValue,
-} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
   databaseURL: "https://realtime-database-8df40-default-rtdb.firebaseio.com/",
@@ -21,18 +16,19 @@ const shoppingListEl = document.getElementById("shopping-list");
 addButtonEl.addEventListener("click", function () {
   let inputValue = inputFieldEl.value;
   push(shoppingListInDB, inputValue);
- 
-console.log(inputValue);
-  appendItemToShoppingListEl(inputValue);
+  clearInputFieldEl();
 });
 
 onValue(shoppingListInDB, function (snapshot) {
   let itemsArray = Object.values(snapshot.val());
-
+  clearShoppingListEl();
   for (let i = 0; i < itemsArray.length; i++) {
     appendItemToShoppingListEl(itemsArray[i]);
   }
 });
+function clearShoppingListEl() {
+  shoppingListEl.innerHTML = "";
+}
 
 function clearInputFieldEl() {
   inputFieldEl.value = "";
