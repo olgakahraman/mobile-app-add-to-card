0f5417eae4103cf6bef6b15c3dbc3d1dd7971600
olgakahraman <ol.khrmn@gmail.com>
2023-07-12 18:27:31 -0400

    solving problems

--- index.js
+++ index.js
@@ -3,37 +3,36 @@ import {
   getDatabase,
   ref,
   push,
-  onValue
+  onValue,
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
-  databaseURL: "https://realtime-database-812db-default-rtdb.firebaseio.com/",
+  databaseURL: "https://realtime-database-8df40-default-rtdb.firebaseio.com/",
 };
 
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
 const shoppingListInDB = ref(database, "shoppingList");
 
-const addButtonEl = document.querySelector("#add-button");
-const inputFieldEl = document.querySelector("#input-field");
-const shoppingListEl = document.querySelector("#shopping-list");
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+const shoppingListEl = document.getElementById("shopping-list");
 
 addButtonEl.addEventListener("click", function () {
   let inputValue = inputFieldEl.value;
   push(shoppingListInDB, inputValue);
-  console.log(`${inputValue} added to database`);
-  clearInputFieldEl();
+ 
+console.log(inputValue);
   appendItemToShoppingListEl(inputValue);
 });
 
-onValue(shoppingListInDB, function(snapshot){
-let itemsArray = Object.values(snapshot.val());
-for( let i = 0; i < itemsArray.length; i++){
-  
-  appendItemToShoppingListEl(itemsArray[i]);
-}
-})
+onValue(shoppingListInDB, function (snapshot) {
+  let itemsArray = Object.values(snapshot.val());
 
+  for (let i = 0; i < itemsArray.length; i++) {
+    appendItemToShoppingListEl(itemsArray[i]);
+  }
+});
 
 function clearInputFieldEl() {
   inputFieldEl.value = "";
